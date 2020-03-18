cc9136b6b3e1d9690e4b935ce0a1ebce1c6356a6
renyh <1025900651@qq.com>
2020-03-18 13:31:16 +0800

    添加文件：mutation-types

---
+++ src/store/modules/mutation-types.js
@@ -0,0 +1,9 @@
+export const RESET_STATE = 'RESET_STATE'  //重置状态
+export const SET_TOKEN = 'SET_TOKEN'  //设置token
+export const SET_NAME = 'SET_NAME'  //设置名称
+export const SET_AVATAR = 'SET_AVATAR'  //设置avatar
+export const SET_DATE = 'SET_DATE'  //设置日期
+export const CHANGE_SETTING = 'CHANGE_SETTING'
+export const TOGGLE_SIDEBAR = 'TOGGLE_SIDEBAR'
+export const CLOSE_SIDEBAR = 'CLOSE_SIDEBAR'
+export const TOGGLE_DEVICE = 'TOGGLE_DEVICE'
